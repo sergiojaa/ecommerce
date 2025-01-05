2e3477b82227be0a7e1ce729de9d1b6fca3f23b8
Sergi kaliashvili <kaliashvilisergi@gmail.com>
2025-01-05 17:54:34 +0400

    Update AdminSidebar.tsx

--- app/components/admin/AdminSidebar.tsx
+++ app/components/admin/AdminSidebar.tsx
@@ -25,7 +25,7 @@ export default function AdminSidebar() {
             <hr className='w-[200px]' />
             <li className='flex items-center gap-4 cursor-pointer'>
                 <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.5812 1.16992H2.94484C2.10804 1.16992 1.42969 1.85393 1.42969 2.6977V11.8644C1.42969 12.7081 2.10804 13.3921 2.94484 13.3921H16.5812C17.418 13.3921 18.0964 12.7081 18.0964 11.8644V2.6977C18.0964 1.85393 17.418 1.16992 16.5812 1.16992Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M1.42969 5.61426H18.0964" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path></svg>
-                ჩემი ბარათები
+                მომხმარებელი
             </li>
             <hr className='w-[200px]' />
             <Link href={'/address'}>
