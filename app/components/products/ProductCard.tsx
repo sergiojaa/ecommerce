a4301c40d0e06db25c61e0fe912a5b72f56594a0
Lasha Bidzinashvili <116507932+Bidzinashvili@users.noreply.github.com>
2025-01-31 03:54:55 +0400

    Search by Price

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,7 +1,10 @@
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons/faCartShopping';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react'
+import { checkTokenValidity } from '../utils/checkTokenValidity';
 
 
 export type productType = {
@@ -15,11 +18,57 @@ export type productType = {
 
 type ProductCardProps = {
   product: productType;
-  addtocart: (id: string) => void; // Assuming `addtocart` is a function that takes an id as a parameter
   loadingProduct: string | null; // Assumes loadingProduct is either a string (the ID of a product) or null
+  setLoadingProduct: (id: string | null) => void
 }
 
-export default function ProductCard({ product, addtocart, loadingProduct }: ProductCardProps) {
+export default function ProductCard({ product, loadingProduct, setLoadingProduct }: ProductCardProps) {
+  const router = useRouter()
+
+  const addtocart = async (id: string) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const tokenValidity = await checkTokenValidity(token)
+      console.log(tokenValidity)
+
+      if (!tokenValidity) {
+        router.push('/login');
+        return;
+      }
+    } else {
+      return router.push('/login')
+    }
+
+
+    setLoadingProduct(id);
+
+    const MIN_LOADING_TIME = 1000;
+    const startTime = Date.now();
+
+    axios
+      .post(
+        'http://localhost:3001/cart/add-to-cart',
+        { productId: id },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log('Product added to cart:', res.data);
+      })
+      .catch((err) => {
+        console.error('Error adding product to cart:', err);
+      })
+      .finally(() => {
+        const elapsedTime = Date.now() - startTime;
+        const remainingTime = Math.max(0, MIN_LOADING_TIME - elapsedTime);
+        setTimeout(() => setLoadingProduct(null), remainingTime);
+      });
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="border w-[250px] p-4 rounded shadow  flex flex-col justify-between">
