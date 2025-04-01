fd12740bf390e0516b64750531f5304cef5b5ed2
seriojaa <kaliashvilisergi@gmail.com>
2025-04-01 21:26:49 +0400

    Fix Size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -23,7 +23,7 @@ const categories = [
 export default function Footer() {
   return (
     <footer className='bg-secondary text-white px-[20px] md:px-[100px] py-[70px] mt-[100px]'>
-      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-[40px] md:gap-[70px] justify-center'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[40px] md:gap-[70px] justify-center'>
 
         <div className='w-full'>
           <h1 className='font-bold text-[18px]'>RGR Group</h1>
